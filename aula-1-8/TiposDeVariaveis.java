781f2b4bde750d22960224aa18fe9e1c5a33411c
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-13 19:08:10 -0300

    exercicio: 1-8

---
+++ aula-1-8/TiposDeVariaveis.java
@@ -0,0 +1,17 @@
+class TiposDeVariaveis {
+  public static void main(String[] args) {
+    byte bite = 127;
+    short numeroCurto = 32767;
+    long numeroLongo = 92233720;
+    int numero = 214748364;
+    char letrasAscii = 111;
+    double numeroComPontos = 1.56;
+    float numeroComPontoSetado = 1.56f;
+
+    boolean VERDADE = true;
+    boolean FALSO = false;
+
+    String mensagem = "Hello world";
+    System.out.println(mensagem);
+  }
+}
