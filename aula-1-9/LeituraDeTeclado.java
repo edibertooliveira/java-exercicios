1e731c4a9a17e4ad9608a643a79ab86db310eefb
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-13 20:13:56 -0300

    exercicio: 1-9

---
+++ aula-1-9/LeituraDeTeclado.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class LeituraDeTeclado {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    /* System.out.println("Escreva seu nome: ");
+    String nomeDigitado = scan.next();
+    System.out.println("Seu nome é " + nomeDigitado); */
+    
+    System.out.println("Digite um numero: ");
+    int primeiroNumero = scan.nextInt();
+    System.out.println("Digite um numero: ");
+    int segundoNumero = scan.nextInt();
+    int result = primeiroNumero + segundoNumero;
+    System.out.println("resultado é: " + result);
+  }
+}
