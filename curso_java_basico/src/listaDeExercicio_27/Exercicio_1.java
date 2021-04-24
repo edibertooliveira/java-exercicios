e03614814c1c8f0e38a056a508dace9a0bfc65db
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-24 18:22:25 -0300

    feat: added exercicio_27 -  01 a 02

---
+++ curso_java_basico/src/listaDeExercicio_27/Exercicio_1.java
@@ -0,0 +1,29 @@
+package listaDeExercicio_27;
+
+/**
+ * Exercicio_1 Escreva uma classe para representar uma lâmpada. Desenvolva
+ * métodos
+ * para ligar, desligar a lampada.
+ */
+public class Exercicio_1 {
+
+  public static void main(String[] args) {
+    Lampada lampadaDoTeto = new Lampada();
+    
+    lampadaDoTeto.modelo = "A60";
+    lampadaDoTeto.tensao = "Bivolt";
+    lampadaDoTeto.garantia = 36;
+    lampadaDoTeto.potencia = 7;
+    lampadaDoTeto.tipo = new String[1];
+    lampadaDoTeto.tipo[0] = "Abaju";
+    lampadaDoTeto.tipo[0] = "Lampada";
+
+    lampadaDoTeto.mudaEstado();
+    System.out.println("Lampada esta " + 
+    lampadaDoTeto.obterEstado());
+
+    lampadaDoTeto.mudaEstado();
+    System.out.println("Lampada esta " + 
+    lampadaDoTeto.obterEstado());
+  }
+}
---
+++ curso_java_basico/src/listaDeExercicio_27/Exercicio_2.java
@@ -0,0 +1,36 @@
+package listaDeExercicio_27;
+
+/**
+ * Exercicio_2 Cria uma classe para representar uma conta corrente que possui um
+ * número, um saldo, um status que informa se ela é especial ou não, um limite.
+ * Desenvolva métodos para realizar saque (verificando se o cliente pode
+ * realizar saques), despositar dinheiro, consultar saldo e verificar se o
+ * cliente está usando cheque especial ou não. Desenvolva um programa para
+ * testar essa classe.
+ * 
+ */
+public class Exercicio_2 {
+
+  public static void main(String[] args) {
+    ContaCorrente cc01 = new ContaCorrente();
+    cc01.numero = "646871";
+    cc01.agencia = "0624";
+    cc01.especial = true;
+    cc01.limiteEspecial = 500;
+    cc01.saldo = 500;
+    boolean saque520 = cc01.realizarSaque(520);
+
+    System.out.println("Saque " + (saque520 ? "Realizado com sucesso" : "Rejeitado pela financeira"));
+
+    System.out.println("Seu saldo é de " + cc01.obterSaldo());
+    cc01.realizarDeposito(200);
+    System.out.println("Seu saldo é de " + cc01.obterSaldo());
+    
+    if (cc01.verificarUsoChequeEspecial()) {
+      System.out.println("Esta usando cheque especial");
+    } else {
+      System.out.println("Não esta usando cheque especial");
+    }
+  }
+
+}
