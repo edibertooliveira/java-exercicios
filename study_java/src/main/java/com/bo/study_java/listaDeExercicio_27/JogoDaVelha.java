0d5dd87a46fe3a370913f5d1acf4d3a66a32a3c5
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-26 18:44:42 -0300

    feat: added exercicio_27 -  03 a 04

---
+++ study_java/src/main/java/com/bo/study_java/listaDeExercicio_27/JogoDaVelha.java
@@ -0,0 +1,56 @@
+package com.bo.study_java.listaDeExercicio_27;
+
+public class JogoDaVelha {
+  
+  private char[][] jogoVelha;
+  private int jogada;
+
+  public JogoDaVelha(char[][] jogoVelha, int jogada) {
+    this.jogoVelha = jogoVelha;
+    this.jogada = jogada;
+  }
+
+  public JogoDaVelha() {
+    jogoVelha = new char[3][3];
+    jogada = 1;
+  }
+
+  boolean vezJogador1() {
+    if (jogada % 2 == 1) { 
+      return true;
+    } 
+    return false;
+  }
+
+  boolean validarJogada(int linha, int coluna, char sinal) {
+    if (jogoVelha[linha][coluna] == 'X' || jogoVelha[linha][coluna] == 'O') {
+      return false;
+    }
+    jogoVelha[linha][coluna] = sinal;
+    jogada++;
+    return true;
+  }
+
+  void imprimirTabuleiro() {
+    for (int i = 0; i < jogoVelha.length; i++) {
+      for (int j = 0; j < jogoVelha[i].length; j++) {
+        System.out.print(jogoVelha[i][j] + " | ");
+      }
+      System.out.println();
+    }
+  }
+
+  boolean verificarGanhador(char sinal) {
+    if ((jogoVelha[0][0] == sinal && jogoVelha[0][1] == sinal && jogoVelha[0][2] == sinal) ||
+    (jogoVelha[1][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[1][2] == sinal) || 
+    (jogoVelha[2][0] == sinal && jogoVelha[2][1] == sinal && jogoVelha[2][2] == sinal) || 
+    (jogoVelha[0][0] == sinal && jogoVelha[1][0] == sinal && jogoVelha[2][0] == sinal) || 
+    (jogoVelha[0][1] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][1] == sinal) || 
+    (jogoVelha[0][2] == sinal && jogoVelha[1][2] == sinal && jogoVelha[2][2] == sinal) || 
+    (jogoVelha[0][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][2] == sinal) ||
+    (jogoVelha[0][2] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][0] == sinal)) {
+      return true;
+    }
+    return false;
+  }
+}
